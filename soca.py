3a6c10c70a63dde713b91d09198138498bcd5c66
Rittik Halder <112501258+forittik@users.noreply.github.com>
2024-09-11 23:34:49 +0530

    Update soca.py

--- soca.py
+++ soca.py
@@ -35,7 +35,7 @@ st.markdown("""
     }
     .stSelectbox>div>div>div[data-baseweb="select"]>div[data-option="Optics"],
     .stSelectbox>div>div>div[data-baseweb="select"]>div[data-option="Strength"] {
-        color: #ffffff;
+        color: #000000;
     }
     .stMarkdown {
         color: #000000;
