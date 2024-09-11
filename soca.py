598f32d3be342b4e90ae5ba372fa9a7e1448214e
Rittik Halder <112501258+forittik@users.noreply.github.com>
2024-09-12 00:12:22 +0530

    Update soca.py

--- soca.py
+++ soca.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="📊 Advanced Data Analysis App", layout="wide")
 st.markdown("""
     <style>
     .main {
-        background-color: #39FF14;
+        background-color: #D3D3D3;
         color: #000000;
     }
     .sidebar .sidebar-content {
